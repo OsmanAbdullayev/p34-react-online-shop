18c4400d445ca7a2f4177b8a05947c02fc1b5c4d
Osman Abdullayev <os.abdullayev@gmail.com>
2022-11-02 21:30:14 +0400

    deleted useNavigate that was not used

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { React, useState } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "react-use-cart";
 import Navbar from "../components/Navbar.jsx";
 import Home from "../pages/Home";
